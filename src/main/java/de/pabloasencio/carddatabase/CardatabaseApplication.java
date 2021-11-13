0e25ab1de4cef15ef0dd4538e27db01158191469
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2021-11-13 11:08:29 +0100

    feat: Create CommandLineRunner to add data to the database

--- src/main/java/de/pabloasencio/carddatabase/CardatabaseApplication.java
+++ src/main/java/de/pabloasencio/carddatabase/CardatabaseApplication.java
@@ -1,17 +1,38 @@
 package de.pabloasencio.carddatabase;
 
+import de.pabloasencio.carddatabase.domain.Car;
+import de.pabloasencio.carddatabase.domain.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
+	private CarRepository carRepository;
+
+	public CardatabaseApplication(CarRepository carRepository) {
+		this.carRepository = carRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2107, BigDecimal.valueOf(59000)));
+			carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, BigDecimal.valueOf(29000)));
+			carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, BigDecimal.valueOf(39000)));
+		};
 	}
 
 }
--- src/main/java/de/pabloasencio/carddatabase/domain/CarRepository.java
+++ src/main/java/de/pabloasencio/carddatabase/domain/CarRepository.java
@@ -1,6 +1,8 @@
 package de.pabloasencio.carddatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends CrudRepository<Car, Long> {
 }
