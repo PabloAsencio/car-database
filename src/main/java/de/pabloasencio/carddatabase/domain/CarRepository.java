e4f6663b3fc53b94b359bf608f7c4c06d26f512b
Pablo Asencio <8017043+PabloAsencio@users.noreply.github.com>
2021-11-15 21:35:24 +0100

    chore: Set up Spring Security dependency in pom.xml

--- src/main/java/de/pabloasencio/carddatabase/domain/CarRepository.java
+++ src/main/java/de/pabloasencio/carddatabase/domain/CarRepository.java
@@ -1,8 +1,15 @@
 package de.pabloasencio.carddatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+import java.util.List;
+
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    List<Car> findByColor(@Param("color") String color);
 }
